a6a2fa7d0a1608ea9c136ee1de204b9cc0477528
RameshRavone <ramesh.maran443@gmail.com>
2018-09-27 16:50:03 +0530

    Fix path sep problem

--- config.py
+++ config.py
@@ -15,81 +15,88 @@
 """
 
 build = {
-"admob"		: True,
-"adcolony"	: True,
-"chartboost"	: True,
-"vungle"	: True,
-"mopub"		: True,
-"unity_ads"	: True,
-"awesome_ads"	: True,
-"appodeal"	: False,
-"inmobi"	: False,
+"admob"         : True,
+"adcolony"      : True,
+"chartboost"    : True,
+"vungle"        : True,
+"mopub"         : True,
+"unity_ads"     : True,
+"awesome_ads"   : True,
+"appodeal"      : False,
+"inmobi"        : False,
 }
-
 
 import os
 
 def can_build(plat):
-	return (plat == "android")
+    return (plat == "android")
+    #return False
 
 def configure(env):
-	cur_dir = os.path.dirname(os.path.abspath(__file__))
+    cur_dir = os.path.dirname(os.path.abspath(__file__)).replace(os.path.sep, '/')
+    libpath = os.path.join(cur_dir, "libs").replace(os.path.sep, '/')
+    respath = os.path.join(cur_dir, "res").replace(os.path.sep, '/')
 
-	if env["platform"] == "android":
-		env.android_add_maven_repository('url "https://maven.google.com"')
-		env.android_add_maven_repository(\
-		"url 'https://oss.sonatype.org/content/repositories/snapshots'")
+    if env["platform"] == "android":
+        env.android_add_maven_repository('url "https://maven.google.com"')
+        env.android_add_maven_repository(\
+            "url 'https://oss.sonatype.org/content/repositories/snapshots'")
 
-		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
-		env.android_add_to_permissions("android/AndroidPermissionsChunk.xml");
+        env.android_add_to_manifest("android/AndroidManifestChunk.xml")
+        env.android_add_to_permissions("android/AndroidPermissionsChunk.xml");
 
-		#env.android_add_gradle_classpath("com.google.gms:google-services:3.1.1")
-		#env.android_add_gradle_plugin("com.google.gms.google-services")
+        #env.android_add_gradle_classpath("com.google.gms:google-services:3.1.1")
+        #env.android_add_gradle_plugin("com.google.gms.google-services")
 
-		if "FireBase" in env.module_list: pass
-		else: env.android_add_java_dir("utils");
+        if "FireBase" in env.module_list: pass
+        else: env.android_add_java_dir("utils");
 
-		env.android_add_java_dir("android");
-		env.android_add_res_dir("res");
+        env.android_add_java_dir("android");
+        env.android_add_res_dir("res");
 
-		env.android_add_dependency("compile 'com.google.android.gms:play-services-ads:11.6.0'")
+        env.android_add_dependency("compile 'com.google.android.gms:play-services-ads:15.0.1'")
 
-		if (build["admob"]):
-			env.android_add_java_dir("admob");
-		if (build["adcolony"]):
-			env.android_add_java_dir("adcolony");
-			env.android_add_to_manifest("adcolony/AndroidManifestChunk.xml")
+        if (build["admob"]):
+            env.android_add_java_dir("admob");
+        if (build["adcolony"]):
+            env.android_add_java_dir("adcolony");
+            env.android_add_to_manifest("adcolony/AndroidManifestChunk.xml")
 
-			env.android_add_maven_repository('url "https://adcolony.bintray.com/AdColony"')
-			env.android_add_dependency("compile 'com.android.support:support-annotations:25.0.1'")
-			env.android_add_dependency("compile 'com.adcolony:sdk:3.2.1'")
+            env.android_add_maven_repository('url "https://adcolony.bintray.com/AdColony"')
+            env.android_add_dependency("compile 'com.android.support:support-annotations:25.0.1'")
+            env.android_add_dependency("compile 'com.adcolony:sdk:3.2.1'")
 
-		if (build["chartboost"]):
-			env.android_add_java_dir("chartboost");
-			env.android_add_to_manifest("chartboost/AndroidManifestChunk.xml")
+        if (build["chartboost"]):
+            env.android_add_java_dir("chartboost");
+            env.android_add_to_manifest("chartboost/AndroidManifestChunk.xml")
 
-			env.android_add_dependency("compile fileTree(dir: '"+cur_dir+"/libs', include: ['*.jar'])")
-		if (build["vungle"]):
-			env.android_add_java_dir("vungle");
-			env.android_add_to_manifest("vungle/AndroidManifestChunk.xml")
+            env.android_add_dependency("compile fileTree(dir: '"+libpath+"', include: ['*.jar'])")
 
-			env.android_add_dependency("compile 'com.google.android.gms:play-services-location:11.6.0'")
-			env.android_add_dependency("compile fileTree(dir: '"+cur_dir+"/libs', include: ['*.jar'])")
-		if (build["mopub"]):
-			env.android_add_default_config("minSdkVersion 16")
+        if (build["vungle"]):
+            env.android_add_java_dir("vungle");
+            env.android_add_to_manifest("vungle/AndroidManifestChunk.xml")
 
-			env.android_add_java_dir("mopub");
-			env.android_add_res_dir("mopub/res");
-			env.android_add_to_manifest("mopub/AndroidManifestChunk.xml")
+            env.android_add_dependency("compile 'com.google.android.gms:play-services-location:11.6.0'")
+            env.android_add_dependency("compile fileTree(dir: '"+libpath+"', include: ['*.jar'])")
 
-			env.android_add_maven_repository('url "https://s3.amazonaws.com/moat-sdk-builds"')
-			env.android_add_dependency("compile('com.mopub:mopub-sdk:4.16.0@aar') { transitive = true }")
-		if (build["unity_ads"]):
-			env.android_add_java_dir("unity_ads");
-			env.android_add_flat_dir(cur_dir + "/libs")
-			env.android_add_flat_dir(cur_dir + "/res")
-			env.android_add_dependency("compile(name:'unity-ads', ext:'aar')")
-		if (build["awesome_ads"]): pass
-		if (build["appodeal"]): pass
-		if (build["inmobi"]): pass
+        if (build["mopub"]):
+            env.android_add_default_config("minSdkVersion 16")
 
+            env.android_add_java_dir("mopub");
+            env.android_add_res_dir("mopub/res");
+            env.android_add_to_manifest("mopub/AndroidManifestChunk.xml")
+
+            env.android_add_maven_repository('url "https://s3.amazonaws.com/moat-sdk-builds"')
+            env.android_add_dependency("compile('com.mopub:mopub-sdk:4.16.0@aar') { transitive = true }")
+
+        if (build["unity_ads"]):
+            env.android_add_java_dir("unity_ads");
+            env.android_add_flat_dir(libpath)
+            env.android_add_flat_dir(respath)
+            env.android_add_dependency("compile(name:'unity-ads', ext:'aar')")
+
+        if (build["awesome_ads"]): pass
+        if (build["appodeal"]): pass
+        if (build["inmobi"]): pass
+
+    pass
